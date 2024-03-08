52b665d387b22e3111e0b14bc67877963cb3134c
intern2_crosscut <sourav024saha@gmail.com>
2024-03-08 12:42:13 +0530

    Resize Window

---
+++ use-throttle/src/hooks/use-throttle.js
@@ -0,0 +1,30 @@
+import {useState,useRef} from 'react';
+import {useEffect} from 'react';
+const useThrottle =(value,delay) =>{
+const [throttledValue,setThrottledValue]=useState(value,delay);
+
+const lastExecuted =useRef(Date.now())
+
+
+useEffect (() =>{
+   const handler= setTimeout(() =>{
+      const now=Date.now();
+      const timeElapsed= now-lastExecuted.current;
+     
+      if(timeElapsed>=delay){
+        setThrottledValue(value)
+        lastExecuted.current=now;
+      }
+
+   },delay-(Date.now() -lastExecuted.current));
+   return ()=>{
+    clearTimeout(handler);
+   };
+},[delay,value]);
+ 
+
+return throttledValue;
+
+};
+
+export default useThrottle;
